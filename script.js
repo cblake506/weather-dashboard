dccff571c58fcd4628b29846b603563f2cce4eb0
Connor Blake <cblake506@gmail.com>
2022-04-23 15:20:33 -0500

    added api request

---
+++ script.js
@@ -0,0 +1,15 @@
+var apiKey = '&appid=1c27f27a707bdfd5f41ae112c33f6e37';
+var city = 'austin'
+
+var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + apiKey;
+fetch(url, {
+    // The browser fetches the resource from the remote server without first looking in the cache.
+    // The browser will then update the cache with the downloaded resource.
+    cache: 'reload',
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
