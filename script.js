d9fa44cd2d7137852fcdd4fd5dbb7d8be0655f46
Connor Blake <cblake506@gmail.com>
2022-04-23 15:35:03 -0500

    added city input html form

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-var apiKey = '&appid=1c27f27a707bdfd5f41ae112c33f6e37';
-var city = 'austin'
+function getWeather(event){
+    event.preventDefault();
 
-var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + apiKey;
-fetch(url, {
-    // The browser fetches the resource from the remote server without first looking in the cache.
-    // The browser will then update the cache with the downloaded resource.
-    cache: 'reload',
-  })
-    .then(function (response) {
-      return response.json();
+    var apiKey = '&appid=1c27f27a707bdfd5f41ae112c33f6e37';
+    // var city = document.getElementById('city').textContent;
+    var city = 'austin';
+
+    var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + apiKey;
+    fetch(url, {
+        // The browser fetches the resource from the remote server without first looking in the cache.
+        // The browser will then update the cache with the downloaded resource.
+        cache: 'reload',
     })
-    .then(function (data) {
-      console.log(data);
-    });
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+        console.log(data);
+        });
+}
+
+document.getElementById('city-button').addEventListener('click', getWeather)
